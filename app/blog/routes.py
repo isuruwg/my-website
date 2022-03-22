228af68c9101fd872a9f155ee707d5ed942eea7e
Isuru Gunasekara <isuru@isuru.ca>
2022-03-21 22:42:50 -0400

    added root page and reorganized pages

--- app/blog/routes.py
+++ app/blog/routes.py
@@ -4,7 +4,7 @@ from flask import render_template
 
 @blog.route("/blog")
 def blog_root_page():
-    return render_template("blog.html", page='markdown/blog_index.html')
+    return render_template("blog.html", page='blog_index.html')
 
 @blog.route("/blog/<blog_page>")
 def blog_content_pages(blog_page):
