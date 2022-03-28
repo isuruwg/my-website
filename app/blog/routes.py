8f825dc87b188e1063373cdf200e56d8ed11d6cc
Isuru Gunasekara <isuru@isuru.ca>
2022-03-28 14:39:55 -0400

    Added error handling when blog page route is invalid

--- app/blog/routes.py
+++ app/blog/routes.py
@@ -1,5 +1,6 @@
 from app.blog import blog
-from flask import render_template
+from flask import render_template, abort
+from os import path
 
 
 @blog.route("/blog")
@@ -9,4 +10,10 @@ def blog_root_page():
 
 @blog.route("/blog/<blog_page>")
 def blog_content_pages(blog_page):
-    return render_template("blog.html", page='markdown/{}'.format(blog_page))
+    # create the path for the page
+    page_path = 'markdown/{}'.format(blog_page)
+    if not path.isfile(page_path):
+        abort(404)
+
+    return render_template("blog.html", page=page_path)
+
