3909dcccda63b9d1475cfdb0dcc483ba352b8228
isuru <isuru@isuru.ca>
2022-03-31 15:53:22 -0400

    fixed formatting

--- app/blog/routes.py
+++ app/blog/routes.py
@@ -4,9 +4,9 @@ from flask import render_template
 
 @blog.route("/blog")
 def blog_root_page():
-    return render_template("blog.html", page='blog_index.html')
+    return render_template("blog.html", page="blog_index.html")
 
 
 @blog.route("/blog/<blog_page>")
 def blog_content_pages(blog_page):
-    return render_template("blog.html", page='markdown/{}'.format(blog_page))
+    return render_template("blog.html", page="markdown/{}".format(blog_page))
